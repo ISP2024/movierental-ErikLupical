cbfd873b2e192553990c342efd6eaa363626e3e0
Oak Soe Htet <erikbartholemew@gmail.com>
2024-10-19 20:49:08 +0700

    Extract Frequent Renter Points

--- customer.py
+++ customer.py
@@ -40,14 +40,7 @@ class Customer:
         statement += header_fmt.format("Movie Title", "  Days", " Price")
         rental_fmt = "{:40s}  {:6d} {:6.2f}\n"
         
-        for rental in self.rentals:            
-            # compute the frequent renter points based on movie price code
-            if rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
-                # New release earns 1 point per day rented
-                frequent_renter_points += rental.get_days_rented()
-            else:
-                # Other rentals get only 1 point
-                frequent_renter_points += 1
+        for rental in self.rentals:  
             #  add a detail line to statement
             statement += rental_fmt.format(
                             rental.get_movie().get_title(), 
@@ -55,6 +48,7 @@ class Customer:
                             rental.get_price())
             # and accumulate activity
             total_amount += rental.get_price()
+            frequent_renter_points += rental.rental_points()         
 
         # footer: summary of charges
         statement += "\n"
--- rental_test.py
+++ rental_test.py
@@ -34,9 +34,17 @@ class RentalTest(unittest.TestCase):
     	rental = Rental(self.childrens_movie, 5)
     	self.assertEqual(rental.get_price(), 4.5)
 
-    @unittest.skip("add this test of rental points when you add it to Rental")
     def test_rental_points(self):
-    	self.fail("add this test of frequent renter points")
+    	rental = Rental(self.new_movie, 3)
+    	self.assertEqual(rental.rental_points(), 3)
+    	rental = Rental(self.new_movie, 1)
+    	self.assertEqual(rental.rental_points(), 1)
+
+    	rental = Rental(self.regular_movie, 5)
+    	self.assertEqual(rental.rental_points(), 1)
+
+    	rental = Rental(self.childrens_movie, 2)
+    	self.assertEqual(rental.rental_points(), 1)
 
 if __name__ == '__main__':
     unittest.main()
